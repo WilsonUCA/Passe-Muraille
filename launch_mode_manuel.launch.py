a7fbc398aa2b805dac58e5d4907af80b4d1a425b
bruh <your@exemple.cum>
2024-11-07 16:35:58 +0100

    i

--- launch_mode_manuel.launch.py
+++ launch_mode_manuel.launch.py
@@ -1,7 +1,7 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-def generate_lauch_description():
-    return Launchdescription([
+def generate_launch_Description():
+    return LaunchDescription([
     Node(package='ModeManuel', executable='ModeManuel', name='manuel')
     ])
